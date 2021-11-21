ee989544ebe1ae0192343bddb2a46995ed0ba317
pao_cuellar <pao.cuellar18@hotmail.com>
2021-11-21 03:01:17 -0600

    uso de api de facebook, pantallas de victoria y derrota y modificaciones menores a botones y enlaces

---
+++ Pantallas/js/apiFacebook.js
@@ -0,0 +1,31 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '660228742095522',
+      xfbml      : true,
+      version    : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
+  
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk')); 
+  
+
+  function shareScore(score) {
+    
+    FB.ui({
+
+        method: 'feed',
+        hashtag: "#UniverseDefendersGame",
+        quote: "He defendido al universo con un total de: "+ score +" puntos ¿crees que puedes superarme? Entra a https://google.com",
+        picture: 'https://i.postimg.cc/qMtYwrvW/Banner.png',
+        redirect_uri: 'https://developers.facebook.com/docs/dialogs/'
+        //link: 'https://google.com', //Link de nuestra pagina, o pones un link o pones una imagen, no hay dos
+        //redirect_uri: 'https://developers.facebook.com/docs/dialogs/' Se supone que es una redireccion al darle click al link de arriba, se supone, segun yo
+    }, function(response){});
+}
